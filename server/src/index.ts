75e1f26e582fe842e1e02504ad3e393ef4020e5d
Andrew Morris <voltrevo@gmail.com>
2023-04-16 05:13:18 +0900

    Call abi

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import * as ethers from 'ethers';
 
 import config from "../config.json";
+import abi from './abi';
 
 const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
 const wallet = new ethers.Wallet(config.privateKey, provider);
+
+const hyperCartridge = new ethers.Contract(config.hyperCartridgeAddress, abi, wallet);
 
 export default {
   port: 3000,
@@ -13,8 +16,10 @@ export default {
     if (url.pathname === '/deploy') {
       const cartridge = await request.json();
 
+      const res = await hyperCartridge.getState(1);
+
       return new Response(
-        `todo | ${(await wallet.getBalance()).toString()} | ${JSON.stringify(cartridge).length}`
+        `todo | ${JSON.stringify(res)} | ${(await wallet.getBalance()).toString()} | ${JSON.stringify(cartridge).length}`
       );
     }
 
