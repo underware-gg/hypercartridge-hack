25073fa31194008ec0a0e6a39216771ce4d39a26
Andrew Morris <voltrevo@gmail.com>
2023-04-16 07:49:56 +0900

    mint and transfer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,6 @@
+import { exec } from 'child_process';
+import * as fs from 'fs';
+
 import * as ethers from 'ethers';
 
 import config from "../config.json";
@@ -14,13 +17,40 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname === '/deploy') {
-      const cartridge = await request.json();
+      const { address, cartridge } = await request.json();
 
-      const res = await hyperCartridge.getState(1);
+      fs.writeFileSync('cartridge.json', JSON.stringify(cartridge));
 
-      return new Response(
-        `todo | ${JSON.stringify(res)} | ${(await wallet.getBalance()).toString()} | ${JSON.stringify(cartridge).length}`
-      );
+      const ipfsRes = JSON.parse(await new Promise((resolve, reject) => {
+        exec(
+          `curl -X POST -F file=@cartridge.json -u "${config.infura.id}:${config.infura.secret}" "https://ipfs.infura.io:5001/api/v0/add"`,
+          (err, stdout, stderr) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(stdout);
+            }
+          }
+        );
+      }));
+
+      const cartridgeUrl = `${config.ideUrl}/#/${ipfsRes.Hash}`;
+
+      console.log("minting");
+      await (await hyperCartridge.mint(wallet.address, cartridgeUrl)).wait();
+      console.log("minted");
+      const tokenId = (await hyperCartridge.totalSupply()).toNumber();
+
+      console.log("transferring");
+      await (await hyperCartridge.transferFrom(wallet.address, address, tokenId)).wait();
+      console.log("transferred");
+
+      return new Response(JSON.stringify({
+        chainId: provider.network.chainId,
+        nftAddress: config.hyperCartridgeAddress,
+        tokenId,
+        state: cartridgeUrl,
+      }));
     }
 
     return new Response('not found', { status: 404 });
