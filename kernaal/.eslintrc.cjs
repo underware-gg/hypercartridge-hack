a9e4b39445768802d64f491fd2d11fcafff43318
Andrew Morris <voltrevo@gmail.com>
2023-04-15 12:16:40 +0900

    Eslint, tidying up

---
+++ kernaal/.eslintrc.cjs
@@ -0,0 +1,48 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'eslint-plugin-prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'indent': [
+      'error',
+      2,
+    ],
+    'linebreak-style': [
+      'error',
+      'unix',
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'quote-props': ['error', 'consistent-as-needed'],
+  },
+};
