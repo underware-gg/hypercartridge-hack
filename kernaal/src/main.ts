bc61cb0dde4bc5a81b1cd63eabd3f291c9f04189
Andrew Morris <voltrevo@gmail.com>
2023-04-15 13:42:48 +0900

    Add cursor position

--- kernaal/src/main.ts
+++ kernaal/src/main.ts
@@ -1,23 +1,40 @@
 import { renderText64 } from './Text64Node';
 import './style.css';
 
+const canvasWidth = 80;
+const canvasHeight = 45;
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const appElement = document.querySelector<HTMLDivElement>('#app')!;
 
 const startTime = Date.now();
 
+let cursorPos: [number, number] = [
+  Math.floor(canvasWidth / 2),
+  Math.floor(canvasHeight / 2),
+];
+
+window.addEventListener('mousemove', (e) => {
+  const rect = appElement.getBoundingClientRect();
+
+  cursorPos = [
+    Math.floor(canvasWidth * (e.clientX - rect.left) / rect.width),
+    Math.floor(canvasHeight * (e.clientY - rect.top) / rect.height),
+  ];
+});
+
 async function renderLoop() {
   appElement.textContent = '';
 
   appElement.appendChild(
-    renderApp(Date.now() - startTime),
+    renderApp(Date.now() - startTime, cursorPos),
   );
 
   requestAnimationFrame(renderLoop);
 }
 
-function renderApp(t: number) {
-  return renderText64([80, 45], [
+function renderApp(t: number, cursorPos: [number, number]) {
+  return renderText64([canvasWidth, canvasHeight], [
     {
       pos: [10, 10],
       width: 100,
@@ -34,6 +51,11 @@ function renderApp(t: number) {
         width: 1,
         text: '░',
       } : [],
+    {
+      pos: cursorPos,
+      width: 1,
+      text: '█',
+    },
   ]);
 }
 
