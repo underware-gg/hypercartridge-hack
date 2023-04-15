c93eaa04b836d029604ef9b87ef50e1c2834a8a7
Andrew Morris <voltrevo@gmail.com>
2023-04-16 08:06:04 +0900

    comment about testnet

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -45,7 +45,7 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    chiado: {
+    chiado: { // Gnosis testnet
       provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://rpc.chiadochain.net`),
       network_id: 10200,
       chain_id: 10200,
