15c458b81faf9f5f88624816a404da6f68f43b7a
Andrew Morris <voltrevo@gmail.com>
2023-04-15 13:32:18 +0900

    render loop

--- kernaal/src/main.ts
+++ kernaal/src/main.ts
@@ -4,8 +4,20 @@ import './style.css';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const appElement = document.querySelector<HTMLDivElement>('#app')!;
 
-appElement.appendChild(
-  renderText64([80, 45], [
+const startTime = Date.now();
+
+async function renderLoop() {
+  appElement.textContent = '';
+
+  appElement.appendChild(
+    renderApp(Date.now() - startTime),
+  );
+
+  requestAnimationFrame(renderLoop);
+}
+
+function renderApp(t: number) {
+  return renderText64([80, 45], [
     {
       pos: [10, 10],
       width: 100,
@@ -16,5 +28,13 @@ appElement.appendChild(
       width: 1,
       text: 'yperCartridge',
     },
-  ]),
-);
+    Math.floor(t / 500) % 2 === 0 ? 
+      {
+        pos: [15, 15],
+        width: 1,
+        text: '░',
+      } : [],
+  ]);
+}
+
+requestAnimationFrame(renderLoop);
