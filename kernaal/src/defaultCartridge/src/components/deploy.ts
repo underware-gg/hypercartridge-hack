505360399a2e15aa038bc110f5fd395f3083728c
Chromuhlink <Chromuh@protonmail.com>
2023-04-15 20:36:10 +0900

    update deploy.ts

--- kernaal/src/defaultCartridge/src/components/deploy.ts
+++ kernaal/src/defaultCartridge/src/components/deploy.ts
@@ -1,14 +1,14 @@
-import { type Text64Node } from "../Text64Node";
-import { renderFooterNav } from "./footerNav";
+import { type Text64Node } from '../Text64Node';
+import { renderFooterNav } from './footerNav';
 
-export function renderDeploy(): Text64Node { 
-    return [
-        {
-            pos: [10, 10],
-            width: 100,
-            text: 'Deploy',
-        },
-        renderFooterNav(['[U]p', '[D]own', 'Enter']),
-    ];
+export function renderDeploy(time: number, cursorPos: [number, number]): Text64Node { 
+  return [
+    {
+      pos: [10, 10],
+      width: 100,
+      text: 'Deploy',
+    },
+    renderFooterNav(['[U]p', '[D]own', 'Enter']),
+  ];
 
 }
