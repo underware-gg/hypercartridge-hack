dcb22b898e442abb9524dc7a28e524b55db07725
Andrew Morris <voltrevo@gmail.com>
2023-04-15 14:51:12 +0900

    Add valuescript

--- kernaal/src/main.ts
+++ kernaal/src/main.ts
@@ -1,9 +1,12 @@
 import Text64Node, { renderText64 } from './Text64Node';
 import './style.css';
 import { renderTitle } from './components/title';
+import VslibPool from './vslib/VslibPool';
 
 const canvasWidth = 80;
 const canvasHeight = 45;
+
+const pool = new VslibPool();
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const appElement = document.querySelector<HTMLDivElement>('#app')!;
@@ -25,16 +28,22 @@ window.addEventListener('mousemove', (e) => {
 });
 
 async function renderLoop() {
+  const newRender = renderText64([canvasWidth, canvasHeight], await renderApp(Date.now() - startTime, cursorPos));
   appElement.textContent = '';
-
-  appElement.appendChild(
-    renderText64([canvasWidth, canvasHeight], renderApp(Date.now() - startTime, cursorPos)),
-  );
+  appElement.appendChild(newRender);
 
   requestAnimationFrame(renderLoop);
 }
 
-function renderApp(t: number, cursorPos: [number, number]): Text64Node {
+async function renderApp(t: number, cursorPos: [number, number]): Promise<Text64Node> {
+  const result = await pool.run('/main.ts', {
+    '/main.ts': `
+      export default function main() {
+        return 1 + 1;
+      }
+    `,
+  }).wait();
+
   return [
     {
       pos: [10, 10],
@@ -58,6 +67,11 @@ function renderApp(t: number, cursorPos: [number, number]): Text64Node {
       text: '█',
     },
     renderTitle(t, cursorPos),
+    {
+      pos: [20, 25],
+      width: 100,
+      text: `1 + 1 is ${JSON.stringify(result.output)}`,
+    },
   ];
 }
 
