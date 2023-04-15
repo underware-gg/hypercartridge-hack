d86e9800f9e9ab724c13ebbff0d8911b24fe5736
Andrew Morris <voltrevo@gmail.com>
2023-04-16 03:30:55 +0900

    Start webserver

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello from bun!");
+import * as ethers from 'ethers';
+
+import config from "../config.json";
+
+const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl);
+const wallet = new ethers.Wallet(config.privateKey, provider);
+
+export default {
+  port: 3000,
+  async fetch(request: Request) {
+    const url = new URL(request.url);
+
+    if (url.pathname === '/deploy') {
+      const cartridge = await request.json();
+
+      return new Response(
+        `todo | ${(await wallet.getBalance()).toString()} | ${JSON.stringify(cartridge).length}`
+      );
+    }
+
+    return new Response('not found', { status: 404 });
+  },
+};
